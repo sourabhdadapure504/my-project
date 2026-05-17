871569635fd0908ed1d5a31ef83938555a9ec220
Sourabh Dadapure <sourabhdadapure538@gmail.com>
2026-05-17 23:10:52 +0530

    fix api url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'http://13.60.31.151:5000/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 30000,
 });
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://13.60.31.151:5000',
         changeOrigin: true,
       }
     }
